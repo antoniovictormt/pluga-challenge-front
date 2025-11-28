057c3a1a74dbf5e6a36fdb41993044ea22a0121b
antonio-victor <antoniovictormt@gmail.com>
2025-11-28 10:46:30 -0300

    PCF-21 feat: add and configure jest for unit tests

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "jest"
+import nextJest from "next/jest"
+
+const createJestConfig = nextJest({
+    dir: "./"
+})
+
+const config: Config = {
+    clearMocks: true,
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    coverageProvider: "v8",
+    testEnvironment: "jsdom",
+    setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
+    testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+    moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/src/$1"
+    }
+}
+
+export default createJestConfig(config)
---
+++ src/app/__tests__/Home.test.tsx
@@ -0,0 +1,16 @@
+import "@testing-library/jest-dom"
+
+import { render, screen } from "@testing-library/react"
+
+import Home from "../page"
+
+ 
+describe("Page", () => {
+  it("renders a heading", () => {
+    render(<Home />)
+ 
+    const heading = screen.getByText(/Documentation/i)
+ 
+    expect(heading).toBeInTheDocument()
+  })
+})
