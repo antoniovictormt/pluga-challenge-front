3edf5ef31c5923852b4727540ba513fcd161fcca
antonio-victor <antoniovictormt@gmail.com>
2025-12-01 19:22:41 -0300

    PCF-25 style: create svg icon and change to Image component

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import Image from "next/image"
+
 import { Grid } from "@/components/grid"
 import { SearchInput } from "@/components/input-search"
 import { AppModal } from "@/components/modal"
@@ -41,20 +43,13 @@ export function List() {
                     </div>
                 ) : pagedFilteredApps.length === 0 ? (
                     <div className="text-center">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="mb-2 inline size-9"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M15.182 16.318A4.486 4.486 0 0 0 12.016 15a4.486 4.486 0 0 0-3.198 1.318M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z"
-                            />
-                        </svg>
+                        <Image
+                            src="/icons/sad-icon.svg"
+                            alt="Nenhum app encontrado"
+                            width={36}
+                            height={36}
+                            className="mb-2 inline"
+                        />
 
                         <p>{`Nenhum app encontrado para "${search}".`}</p>
                     </div>
