23c86e2011fa7553661ea7a7264374603b2784c9
antonio-victor <antoniovictormt@gmail.com>
2025-12-01 13:35:27 -0300

    PCF-25 feat: create grid component with unit test

---
+++ src/components/grid/index.tsx
@@ -0,0 +1,14 @@
+import { GridProps } from "@/types"
+
+import { Card } from "../card"
+
+export function Grid({ apps, onSelect }: GridProps) {
+    return (
+        <div className="grid gap-6 lg:grid-cols-4">
+            {apps.map(app => (
+                <Card key={app.app_id} app={app} onSelect={onSelect} 
+                 />
+            ))}
+        </div>
+    )
+}
